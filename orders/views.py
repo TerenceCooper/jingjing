14fc164bc85c6c2737ff5f66a5960ff3364ae9c2
TerenceCooper <TerenceCooper@tutamail.com>
2016-10-12 11:50:32 +0800

    Added payment app, views and templates

--- orders/views.py
+++ orders/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.core.urlresolvers import reverse
 
 
 from .models import OrderItem
@@ -19,7 +20,9 @@ def order_create(request):
 										 quantity=item['quantity'])
 			cart.clear()
 			order_created.delay(order.id)
-			return render(request, 'orders/order/created.html',
+			request.session['order_id'] = order.id
+			return redirect(reverse('payment:process'))
+			# return render(request, 'orders/order/created.html',
 							  {'order': order})
 	else:
 		form = OrderCreateForm()
